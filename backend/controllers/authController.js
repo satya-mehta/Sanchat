c74018fff1c8f09aa494e7b9d87cdc69136d852b
Satya Mehta <satyamehta577@gmail.com>
2025-05-26 00:12:00 +0530

    fixed models.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require("../models/User");
+const path = require("path");
+const User = require(path.resolve(__dirname, "../models/User"));
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
