17178e8ab0f550e82958110c4835cedddc702d49
Satya Mehta <satyamehta577@gmail.com>
2025-05-28 00:06:27 +0530

    bug fixing open/public bug for room creation

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const RoomSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
-  type: { type: String, enum: ["public", "protected"], required: true },
+  type: { type: String, enum: ["open", "protected"], required: true },
   secretKey: { type: String, default: null },
   messages: { type: Array, default: [] },
   createdAt: { type: Date, default: Date.now },
